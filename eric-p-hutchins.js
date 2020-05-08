4accb267db9d4f95db79ba7118ae017c9c49eaa8
Eric Hutchins <ehutchins@ravemobilesafety.com>
2020-05-08 09:36:57 -0400

    Update

--- eric-p-hutchins.js
+++ eric-p-hutchins.js
@@ -1,4 +1,33 @@
+import things from './things.js';
+
 class EPH {
-    constructor() {
-    }
+  constructor() {
+  }
+
+  elementWithHTML(tagName, html) {
+    const el = document.createElement(tagName);
+    el.innerHTML = html;
+    return el;
+  }
+
+  h1(html) {
+    return this.elementWithHTML('h1', html);
+  }
+
+  h2(html) {
+    return this.elementWithHTML('h2', html);
+  }
+
+  render() {
+    document.body.appendChild(this.h1('Eric P. Hutchins'));
+    document.body.appendChild(this.h2('Things'));
+    things.forEach(thing => {
+      const div = document.createElement('div');
+      div.classList.add('thing');
+      div.innerHTML = `<a href="${thing.link}">${thing.name}</a>`;
+      document.body.appendChild(div);
+    });
+  }
 }
+
+new EPH().render();
---
+++ things.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    name: 'Page Source',
+    link: 'http://github.com/eric-p-hutchins/eric-p-hutchins.github.io',
+  },
+  {
+    name: 'Code Dream',
+    link: 'http://github.com/eric-p-hutchins/code-dream',
+  },
+]
